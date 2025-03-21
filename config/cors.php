a0738b67e817bf92bf60ac0b86c52fc79363e3c5
Agadyr <kairambekovmadi@gmail.com>
2025-03-21 17:06:03 +0500

    fix

--- config/cors.php
+++ config/cors.php
@@ -1,30 +1,11 @@
 <?php
 
 return [
-    'paths' => ['*'], // Разрешаем все пути
-
+    'paths' => ['api/*', 'sanctum/csrf-cookie'], // Ограничь пути
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => [
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-        'https://localhost:3000',
-        'https://127.0.0.1:3000'
-    ],
-
-    'allowed_headers' => [
-        'Content-Type',
-        'X-Requested-With',
-        'Authorization',
-        'X-XSRF-TOKEN',
-        'X-CSRF-TOKEN',
-        'Accept',
-        'Origin'
-    ],
-
-    'exposed_headers' => ['*'],
-
+    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Authorization', 'X-XSRF-TOKEN', 'X-CSRF-TOKEN', 'Accept', 'Origin'],
+    'exposed_headers' => [],
     'max_age' => 0,
-
     'supports_credentials' => true,
 ];
